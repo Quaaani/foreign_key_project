54b46b6c07d7c123892af6fad3bf59419d3b3df3
Anton Stetsenko <anton2281488@mail.ru>
2022-04-08 17:01:46 +0300

    Photo button User_info adaptted done

--- client/src/components/ProfileStudent/ProfileStudent.jsx
+++ client/src/components/ProfileStudent/ProfileStudent.jsx
@@ -1,10 +1,9 @@
-import { Button, Card, CardActions, CardMedia, Container, Grid } from "@mui/material"
+import { Button, Card, CardActions, CardContent, CardMedia, Container, Grid, Typography } from "@mui/material"
 import { useDispatch, useSelector } from "react-redux";
 
 function ProfileStudent () {
   
   const { session } = useSelector((state) => state.sessionReducer);
-
   console.log('SESSION PROFILE', session);
 
   return (
@@ -17,8 +16,8 @@ function ProfileStudent () {
       >
         <Grid item
           xs={4}
-          sm={8}
-          md={12}
+          sm={4}
+          md={4}
         >
           <Card
             sx={{ maxWidth: 345 }}
@@ -30,11 +29,54 @@ function ProfileStudent () {
               alt="avatar"
             >
             </CardMedia>
-            <CardActions>
-              <Button color="secondary" variant="outlined">Редактировать профиль</Button>
+            <CardActions centered>
+              <Button color="secondary" variant="outlined" >Редактировать профиль</Button>
             </CardActions>
           </Card>
         </Grid>
+        <Grid item
+          sx={{ maxWidth: 345 }}
+          xs={2}
+          sm={2}
+          md={2}
+        >
+          <CardContent>
+            <Typography variant="h6">
+              Name:
+            </Typography>
+            <Typography variant="h6">
+              Last Name:
+            </Typography>
+            <Typography variant="h6">
+              Status:
+            </Typography>
+            <Typography variant="h6">
+              Level:
+            </Typography>
+          </CardContent>
+        </Grid>
+        <Grid item
+          sx={{ maxWidth: 345 }}
+          xs={2}
+          sm={2}
+          md={2}
+        >
+          <CardContent>
+            <Typography variant="h6">
+              {session?.user_firstName[0].toUpperCase() + session?.user_firstName.slice(1)}
+            </Typography>
+            <Typography variant="h6">
+              {session?.user_lastName[0].toUpperCase() + session?.user_lastName.slice(1)}
+            </Typography>
+            <Typography variant="h6">
+              {session?.user_role}
+            </Typography>
+            <Typography variant="h6">
+              {session?.user_level}
+            </Typography>
+          </CardContent>
+        </Grid>
+
       </Grid>
     </Container>
   )
