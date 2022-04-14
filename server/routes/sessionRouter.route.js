ca11ba965519ad6b928e79fc2649ece53b59cbec
Anton Stetsenko <anton2281488@mail.ru>
2022-04-14 17:19:47 +0300

    Try catch Done

--- server/routes/sessionRouter.route.js
+++ server/routes/sessionRouter.route.js
@@ -2,11 +2,15 @@ const router = require('express').Router()
 
 router.route('/')
   .get((req, res) => {
-    console.log('*** /session GET REQUEST ****')
-
-    if (!req.session.user_data) return res.status(400).json({ message: 'Вы не авторизованы' })
-
-    return res.status(200).json({ data: req.session.user_data })
+    try {
+      console.log('*** /session GET REQUEST ****')
+  
+      if (!req.session.user_data) return res.status(400).json({ message: 'Вы не авторизованы' })
+  
+      return res.status(200).json({ data: req.session.user_data })
+    } catch(error) {
+        console.log('session error', error.message)
+    }
   })
 
 module.exports = router
